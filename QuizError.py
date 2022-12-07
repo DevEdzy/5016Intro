2fece62c7bfd97070cc1b11bf18758f6a0ebc18c
Michael Edwards <116780610+DevEdzy@users.noreply.github.com>
2022-12-07 21:28:09 +1300

    Create QuizError.py

---
+++ QuizError.py
@@ -0,0 +1,13 @@
+Hi team, 
+
+Does anyone know why my code below always returns "correct" regardless of the answer provided:
+
+question_one = input("What city in New Zealand has the steepest street in the world? \n"
+                     "a. Auckland \n"
+                     "b. Dunedin \n"
+                     "c. Christchurch \n")
+
+if question_one == "b" or "B":
+    print("correct")
+elif question_one != "b" or "B":
+    print("incorrect")
