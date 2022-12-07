7de67df138bd9ea1d2ed4862820f17546ee3f9c1
Michael Edwards <116780610+DevEdzy@users.noreply.github.com>
2022-12-07 22:37:25 +1300

    If Statement not working
    If statement was always showing correct regardless of the answer. 
    Have been in touch with my course coordinator and this is due to missing parentheses.

--- ErrorCode/QuizError.py
+++ ErrorCode/QuizError.py
@@ -13,3 +13,13 @@ elif question_one != "b" or "B":
     print("incorrect")
 
 # Answered provided by course co-ordinator.
+
+question_one = input("What city in New Zealand has the steepest street in the world? \n"
+                     "a. Auckland \n"
+                     "b. Dunedin \n"
+                     "c. Christchurch \n")
+
+if question_one == ("b" or "B"):
+    print("correct")
+elif question_one != ("b" or "B"):
+    print("incorrect")
