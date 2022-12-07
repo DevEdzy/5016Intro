c45f288ec44e6c92f57a6677ba614c95f320638b
Michael Edwards <116780610+DevEdzy@users.noreply.github.com>
2022-12-07 22:18:45 +1300

    Main method and function

---
+++ resources/Main_Method.py
@@ -0,0 +1,16 @@
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
+
+print("Guru99")
+
+# Guru99 is printed in this case.
+
+# Here is the explanation,
+
+# When Python interpreter reads a source file, it will execute all the code found in it.
+# When Python runs the “source file” as the main program, it sets the special variable (__name__) to have a value (“__main__”).
+# When you execute the main function in python, it will then read the “if” statement and checks whether __name__ does equal to __main__.
+# In Python “if__name__== “__main__” allows you to run the Python files either as reusable modules or standalone programs.'''
