dacfa80f8d990b97bc310540dd3edcb2d5b314ec
Michael Edwards <116780610+DevEdzy@users.noreply.github.com>
2022-12-07 22:24:42 +1300

    Calling same function within a function

--- resources/Recursion.py
+++ resources/Recursion.py
@@ -9,3 +9,20 @@ def recursive1(i=0):
     i+=1 # update the value of i
     return recursive1(i) # return itself with the updated i
 recursive1()
+
+# Another Example
+
+def collect_folders(start, depth=-1)
+    """ negative depths means unlimited recursion """
+    folder_ids = []
+
+    # recursive function that collects all the ids in `acc`
+    def recurse(current, depth):
+        folder_ids.append(current.id)
+        if depth != 0:
+            for folder in getChildFolders(current.id):
+                # recursive call for each subfolder
+                recurse(folder, depth-1)
+
+    recurse(start, depth) # starts the recursion
+    return folder_ids
