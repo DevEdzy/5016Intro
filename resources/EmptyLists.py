6a8aad6338d442209d32ea93d99f0cf80816ebbc
Michael Edwards <116780610+DevEdzy@users.noreply.github.com>
2022-12-08 22:00:48 +1300

    how to check if list is empty

---
+++ resources/EmptyLists.py
@@ -0,0 +1,47 @@
+# empty list
+lst = [1,2,3,4,5]
+
+# evaluating empty list object to False
+if not lst:
+   print('Empty list')
+else:
+   print('List is not Empty ',lst)
+    
+#Output:
+
+('List is not Empty 
+', [1, 2, 3, 4, 5])
+ 
+ 
+ 
+ 
+ # empty list
+lst = []
+
+# Checking whether the list size is equal to 0
+if len(lst) == 0:
+   print('Empty list')
+else:
+   print('Not Empty list')
+ 
+ #Output:
+ 
+ Empty list
+ 
+ 
+ 
+ 
+ # empty list
+lst = []
+
+# Checking whether the list object is equal to [](null list)
+if lst == []:
+   print('Empty list')
+else:
+   print('List is not empty',lst)
+ 
+ #Output:
+ 
+ Empty list
+ 
+ 
